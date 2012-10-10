fc1ff626af38bb15ff08cfa94a0dec972aa603bf
Kishorekumar Neelamegam <indykish@gmail.com>
2012-10-10 18:46:03 +0530

    Initial commit.

---
+++ src/main/java/org/megam/deccanplato/core/api/APICapability.java
@@ -0,0 +1,15 @@
+package org.megam.deccanplato.core.api;
+
+import java.util.List;
+
+public interface APICapability {
+
+	public String title();
+
+	public String description();
+
+	public String version(String versionid);
+
+	public List<APICall> describe(String apicall);
+
+}
---
+++ src/test/java/org/megam/deccanplato/provider/crm/rest/SaleforceAdapterTest.java
@@ -0,0 +1,59 @@
+package org.megam.deccanplato.provider.crm.rest;
+
+import static org.junit.Assert.*;
+
+import org.apache.wink.client.RestClient;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.megam.deccanplato.provider.crm.info.SalesforceCRM;
+
+import org.apache.wink.client.Resource;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class SaleforceAdapterTest {
+
+	private static String access_stuff;
+
+	@BeforeClass
+	public static void setUp() {
+		System.out.println("setUp");
+		RestClient rc = new RestClient();
+		Resource resource = rc
+				.resource("http://localhost:8080/deccanplato/provider/crm");
+		access_stuff = resource.accept("application/json").get(String.class);
+		System.out.println("Access =>" + access_stuff);
+	}
+
+	@Ignore
+	@Test
+	public void testCreateUser() {
+		System.out.println("testCreateUser :" + access_stuff);
+		RestClient rc = new RestClient();
+
+		Resource resource = rc
+				.resource("http://localhost:8080/deccanplato/provider/crm");
+		String response = resource.contentType("application/json")
+				.accept("application/json").post(String.class, access_stuff);
+		System.out.println("testCreateUser :" + response);
+
+	}
+
+	@Test
+	public void testListUser() {
+		System.out.println("testListUser :"+ access_stuff);
+		RestClient rc = new RestClient();
+		Gson gson = new GsonBuilder().create();
+		SalesforceCRM salesforceCRM = gson.fromJson(access_stuff,
+				SalesforceCRM.class);
+		Resource resource = rc
+				.resource("http://localhost:8080/deccanplato/provider/crm/" + salesforceCRM.getAccess_token()
+						 );
+		String response = resource.accept("application/json")
+				.get(String.class);
+		System.out.println("testListUser :"+response);
+
+	}
+}
