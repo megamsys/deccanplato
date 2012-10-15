d558d8a7c1d2e9a958a473aa1b34404d98826681
pandiyaraja <raja.pandiya@megam.co.in>
2012-10-15 11:27:26 +0530

    Added SalesforceAccount Class

---
+++ src/main/java/org/megam/deccanplato/provider/crm/info/SalesforceCrmAccount.java
@@ -0,0 +1,17 @@
+package org.megam.deccanplato.provider.crm.info;
+
+public class SalesforceCrmAccount {
+	
+	String Name;
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+	
+	
+
+}
