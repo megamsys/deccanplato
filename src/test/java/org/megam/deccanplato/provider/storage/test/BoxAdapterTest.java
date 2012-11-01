1b26f89a742baee22d0c1abb32e035155d2579d3
Thomas Alrin <alrin@thomas-work.(none)>
2012-11-01 19:34:14 +0530

    UPD on 1-11

---
+++ src/test/java/org/megam/deccanplato/provider/storage/test/BoxAdapterTest.java
@@ -0,0 +1,86 @@
+/**
+ * “Copyright 2012 Megam Systems”
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+package org.megam.deccanplato.provider.storage.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.megam.deccanplato.provider.ProviderRegistry;
+import org.megam.deccanplato.provider.box.BoxAdapterAccess;
+import org.megam.deccanplato.provider.core.AdapterAccessException;
+import org.megam.deccanplato.provider.core.DataMap;
+import org.megam.deccanplato.provider.core.RequestData;
+import org.megam.deccanplato.provider.core.RequestDataBuilder;
+import org.megam.deccanplato.provider.salesforce.SalesforceAdapterAccess;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * @author alrin
+ *
+ */
+public class BoxAdapterTest {
+	private static RequestData reqData;
+	private RequestDataBuilder rdb;
+	
+	@BeforeClass
+	public static void setUp() throws IOException {
+		System.out.println("setUp");
+		BufferedReader br = null;
+		String inputJsonPath = new File(".").getCanonicalPath()
+				+ java.io.File.separator + "src" + java.io.File.separator
+				+ "test" + java.io.File.separator + "java"
+				+ java.io.File.separator;
+
+		br = new BufferedReader(new FileReader(inputJsonPath + "box.json"));
+
+		StringBuilder strb = new StringBuilder();
+		String currentLine = "";
+
+		while ((currentLine = br.readLine()) != null) {
+			strb.append(currentLine);
+		}
+		//
+		RequestDataBuilder rdb = new RequestDataBuilder(strb.toString());
+		reqData=rdb.data();
+		System.out.println(rdb.data().toString());
+	    
+		GenericApplicationContext ctx = new GenericApplicationContext();
+		XmlBeanDefinitionReader xmlReader = new XmlBeanDefinitionReader(ctx);
+		xmlReader.loadBeanDefinitions(new ClassPathResource(
+				"applicationContext.xml"));
+		ctx.refresh();		
+		ProviderRegistry registry = (ProviderRegistry) ctx
+				.getBean("registry");
+		System.out.println("Provider Registry" + registry.toString());
+		//System.out.println("Provider Registry" + registry.getAdapter("box"));	
+	}
+	 @Test
+		public void testSalesforceAdapterAccess() throws AdapterAccessException{
+			
+			BoxAdapterAccess baa=new BoxAdapterAccess();
+			DataMap dmap=baa.authenticate(reqData.getGeneral());
+			System.out.println(dmap.map().get("instance_url"));
+			System.out.println(dmap.map().get("access_token"));
+			//System.out.println("DMAP"+dmap.toString());
+		}
+
+}
