1f9c8cc75cd43c406e3201c0f8753986040d230e
Kishorekumar Neelamegam <indykish@gmail.com>
2012-11-05 10:20:46 +0530

    Added a Constants class.

---
+++ src/main/java/org/megam/deccanplato/provider/Constants.java
@@ -0,0 +1,38 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+package org.megam.deccanplato.provider;
+
+/**
+ * A global constants which relate to the request and response json.
+ * @author ram
+ *
+ */
+public class Constants {
+
+	private static final String VERSION = "0.1 beta";
+	
+	/**
+	 * The key that are passed over in the security section of the request Json.
+	 */
+	
+	
+	/**
+	 * The key that are passed over in the provider section of the request Json.
+	 */
+	
+	/**
+	 * The key that are passed over in the output section of the request Json. 
+	 */
+}
