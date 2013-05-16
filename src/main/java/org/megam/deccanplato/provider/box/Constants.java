b24d93583314179d62f232f2931e65f83dbf1e4d
vimalsri <vimalsri@vimalsri-System-Product-Name.(none)>
2013-05-16 08:58:40 +0530

    fixed problem for file download in BOX

--- src/main/java/org/megam/deccanplato/provider/box/Constants.java
+++ src/main/java/org/megam/deccanplato/provider/box/Constants.java
@@ -34,6 +34,7 @@ public class Constants {
 	public static final String LIMIT="limit";
 	public static final String OFFSET="offset";
 	public static final String ETAG="etag";
+	public static final String FILE_TYPE="file_type";
 	/**
 	 * User
 	 */
